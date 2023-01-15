8a44b593dd72d6a5f332aca29cab4c16bd08e6a0
nitinmalik384 <nitinmalik384@gmail.com>
2023-01-15 15:57:35 +0530

    initial setup

--- company-portfolio/src/App.js
+++ company-portfolio/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Error404 from "./pages/404error/404Error";
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext();
 
 function App() {
+  const [theme, setTheme] = useState("dark");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme: theme, setTheme: setTheme }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
   );
 }
 
